be48f7655b2c69446b1b53c5e5dade7a4872a407
DevGibby <71801512+DevGibby@users.noreply.github.com>
2021-12-29 18:05:17 -0500

    Update AboutPage.js

--- src/pages/static-pages/AboutPage.js
+++ src/pages/static-pages/AboutPage.js
@@ -6,15 +6,15 @@ export default function About () {
         <StyledAbout >
             <div className="about-wrapper">
                 <h1>About Us</h1>
-                <p><span id="bold">The Real Deal Fitness</span> was created in 2021. Our goal is to bring real health and fitness tips, 
-                clothing and accessories from mulitple diverse sources and opinions. The Real Deal Fitness is comprised of individuals 
+                <p><span id="bold">Veritable</span> was created in 2021. Our goal is to bring real health and fitness tips, 
+                clothing and accessories from mulitple diverse sources and opinions. Veritable  is comprised of individuals 
                 who are passionate about fitness and live the lifestyle.  We hope the information and products we provide server you and your healthy lifestyles!
                 </p>
                 <h1 id="bottomHeader">The Future..</h1>
                 <p>The goal of this website is to bring a community feeling to the health and fitness world. Our websites are undergoing major development to
                 bring a better and more engaging user experience!  Periodically you will find more and more features being added to the site. 
                 </p>
-                <p id="thanks"><span>Thanks,</span>The Real Deal Fitness</p>
+                <p id="thanks"><span>Thanks,</span>Veritable Fitness</p>
             </div>
         </StyledAbout >
     )
